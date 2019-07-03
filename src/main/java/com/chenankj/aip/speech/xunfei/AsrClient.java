67d3a5ce36aa471f92ffeb869b83a7ad22c552be
SherlockJun <woyaonulixuexi87@gmail.com>
2019-07-03 15:29:43 +0800

    commit on 2019-07-03 15:28

--- src/main/java/com/chenankj/aip/speech/xunfei/AsrClient.java
+++ src/main/java/com/chenankj/aip/speech/xunfei/AsrClient.java
@@ -1,84 +1,73 @@
 package com.chenankj.aip.speech.xunfei;
 
 import com.alibaba.fastjson.JSON;
-import com.chenankj.aip.speech.xunfei.dto.ApiResultDto;
-import com.chenankj.aip.speech.xunfei.util.EncryptUtil;
-import com.chenankj.aip.speech.xunfei.util.HttpUtil;
-import com.chenankj.aip.speech.xunfei.util.SliceIdGenerator;
-import com.chenankj.aip.util.ResourceUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.iflytek.msp.cpdb.lfasr.client.LfasrClientImp;
+import com.iflytek.msp.cpdb.lfasr.exception.LfasrException;
+import com.iflytek.msp.cpdb.lfasr.model.LfasrType;
+import com.iflytek.msp.cpdb.lfasr.model.Message;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.SignatureException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * 非实时转写webapi调用demo
- * 此demo只是一个简单的调用示例, 不适合用到实际生产环境中
- * 
- * @author white
+ * 讯飞语音识别client
  *
+ * @author XieJun
  */
 public class AsrClient {
 
-    public static final String LFASR_HOST = "http://raasr.xfyun.cn/api";
-
     /**
-     * TODO 设置appid和secret_key
+     * 讯飞语音识别
+     *
+     * @param path
+     *          文件路径
+     * @return
      */
-    public static final String APPID = "5d148bf0";
-    public static final String SECRET_KEY = "f9876aea3961beb9ca7fd09a41b0cb7c";
+    public static String asr(String path){
 
-    public static final String PREPARE = "/prepare";
-    public static final String UPLOAD = "/upload";
-    public static final String MERGE = "/merge";
-    public static final String GET_RESULT = "/getResult";
-    public static final String GET_PROGRESS = "/getProgress";
-
-    /**
-     * 文件分片大小,可根据实际情况调整
-     */
-    public static final int SLICE_SICE = 10485760;// 10M
-
-    public static String asr(String path) {
         String result = null;
-        File audio = ResourceUtil.getFileFromResources(path);
-        try (FileInputStream fis = new FileInputStream(audio)) {
-            // 预处理
-            String taskId = prepare(audio);
-
-            // 分片上传文件
-            int len = 0;
-            byte[] slice = new byte[SLICE_SICE];
-            SliceIdGenerator generator = new SliceIdGenerator();
-            while ((len =fis.read(slice)) > 0) {
-                // 上传分片
-                if (fis.available() == 0) {
-                    slice = Arrays.copyOfRange(slice, 0, len);
-                }
-                uploadSlice(taskId, generator.getNextSliceId(), slice);
+        try {
+            File audio = new File(path);
+            //校验文件是否存在
+            if(!audio.exists()){
+                //记录日志
+                throw new RuntimeException("录音文件不存在!");
+            }
+            //音频文件大小校验
+            if(getFileSize(audio) > 500*1024*1024){
+                //记录日志
+                throw new RuntimeException("录音文件超过大小!");
+            }
+            //音频文件格式校验
+            if(!LfasrType.LFASR_TELEPHONY_RECORDED_AUDIO.isSupportedAudios(audio)
+                    && !LfasrType.LFASR_STANDARD_RECORDED_AUDIO.isSupportedAudios(audio)){
+                //记录日志
+                throw new RuntimeException("不支持的录音文件格式!");
             }
 
-            // 合并文件
-            merge(taskId);
+            LfasrClientImp lfasrClientImp = new LfasrClientImp();
+            Message message = lfasrClientImp.lfasrUpload(path, LfasrType.LFASR_STANDARD_RECORDED_AUDIO);
+            if (message.getOk() != 0 || StringUtils.isEmpty(message.getData())) {
+                //预处理失败
+                throw new RuntimeException("预处理失败!");
+            }
+            String taskId = message.getData();
 
-            // 轮询获取任务结果
             while (true) {
+                System.out.println("sleep a while,wait for task progress complete!");
                 try {
-                    System.out.println("sleep a while Zzz" );
                     Thread.sleep(20000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                ApiResultDto taskProgress = getProgress(taskId);
+                Message taskProgress = lfasrClientImp.lfasrGetProgress(taskId);
                 if (taskProgress.getOk() == 0) {
                     if (taskProgress.getErr_no() != 0) {
+                        //任务失败
                         System.out.println("任务失败：" + JSON.toJSONString(taskProgress));
                     }
-
                     String taskStatus = taskProgress.getData();
                     if (JSON.parseObject(taskStatus).getInteger("status") == 9) {
                         System.out.println("任务完成！");
@@ -87,162 +76,50 @@ public class AsrClient {
 
                     System.out.println("任务处理中：" + taskStatus);
                 } else {
+                    //获取任务进度失败
                     System.out.println("获取任务进度失败！");
                 }
             }
-            // 获取结果
-            result = getResult(taskId);
-            System.out.println("\r\n\r\n转写结果: " + getResult(taskId));
-        } catch (SignatureException e) {
+
+            //获取结果
+            Message taskResult = lfasrClientImp.lfasrGetResult(taskId);
+            if(taskResult == null){
+                throw new RuntimeException("获取结果接口请求失败!");
+            }
+
+            if(taskResult.getOk() != 0){
+                throw new RuntimeException("获取结果失败!");
+            }
+            result = getContents(taskResult.getData());
+
+        } catch (LfasrException e) {
             e.printStackTrace();
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
         }
         return result;
     }
 
-    /**
-     * 获取每个接口都必须的鉴权参数
-     * 
-     * @return
-     * @throws SignatureException 
-     */
-    public static Map<String, String> getBaseAuthParam(String taskId) throws SignatureException {
-        Map<String, String> baseParam = new HashMap<String, String>();
-        String ts = String.valueOf(System.currentTimeMillis() / 1000L);
-        baseParam.put("app_id", APPID);
-        baseParam.put("ts", ts);
-        baseParam.put("signa", EncryptUtil.HmacSHA1Encrypt(EncryptUtil.MD5(APPID + ts), SECRET_KEY));
-        if (taskId != null) {
-            baseParam.put("task_id", taskId);
+    private static long getFileSize(File file){
+        if(file != null && file.exists()){
+            long length = file.length();
+            return length;
+        }else{
+            return 0;
         }
-
-        return baseParam;
     }
 
-    /**
-     * 预处理
-     * 
-     * @param audio     需要转写的音频
-     * @return
-     * @throws SignatureException 
-     */
-    public static String prepare(File audio) throws SignatureException {
-        Map<String, String> prepareParam = getBaseAuthParam(null);
-        long fileLenth = audio.length();
-
-        prepareParam.put("file_len", fileLenth + "");
-        prepareParam.put("file_name", audio.getName());
-        prepareParam.put("slice_num", (fileLenth/SLICE_SICE) + (fileLenth % SLICE_SICE == 0 ? 0 : 1) + "");
-
-        /********************TODO 可配置参数********************/
-        // 转写类型
-//        prepareParam.put("lfasr_type", "0");
-        // 开启分词
-//        prepareParam.put("has_participle", "true");
-        // 说话人分离
-//        prepareParam.put("has_seperate", "true");
-        // 设置多候选词个数
-//        prepareParam.put("max_alternatives", "2");
-        // 是否进行敏感词检出
-//        prepareParam.put("has_sensitive", "true");
-        // 敏感词类型
-//        prepareParam.put("sensitive_type", "1");
-        // 关键词
-//        prepareParam.put("keywords", "科大讯飞,中国");
-        /****************************************************/
-
-        String response = HttpUtil.post(LFASR_HOST + PREPARE, prepareParam);
-        if (response == null) {
-            throw new RuntimeException("预处理接口请求失败！");
+    private static String getContents(String data){
+        if(StringUtils.isNotBlank(data) && StringUtils.isNotEmpty(data)){
+            JSONArray jsonArray = JSONObject.parseArray(data);
+            JSONObject jsonObject = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < jsonArray.size(); i++) {
+                jsonObject = (JSONObject) jsonArray.get(i);
+                stringBuilder.append(jsonObject.getString("onebest"));
+            }
+            return stringBuilder.toString();
+        }else{
+            return null;
         }
-        ApiResultDto resultDto = JSON.parseObject(response, ApiResultDto.class);
-        String taskId = resultDto.getData();
-        if (resultDto.getOk() != 0 || taskId == null) {
-            throw new RuntimeException("预处理失败！" + response);
-        }
-
-        System.out.println("预处理成功, taskid：" + taskId);
-        return taskId;
     }
 
-    /**
-     * 分片上传
-     * 
-     * @param taskId        任务id
-     * @param slice         分片的byte数组
-     * @throws SignatureException 
-     */
-    public static void uploadSlice(String taskId, String sliceId, byte[] slice) throws SignatureException {
-        Map<String, String> uploadParam = getBaseAuthParam(taskId);
-        uploadParam.put("slice_id", sliceId);
-
-        String response = HttpUtil.postMulti(LFASR_HOST + UPLOAD, uploadParam, slice);
-        if (response == null) {
-            throw new RuntimeException("分片上传接口请求失败！");
-        }
-        if (JSON.parseObject(response).getInteger("ok") == 0) {
-            System.out.println("分片上传成功, sliceId: " + sliceId + ", sliceLen: " + slice.length);
-            return;
-        }
-
-        System.out.println("params: " + JSON.toJSONString(uploadParam));
-        throw new RuntimeException("分片上传失败！" + response + "|" + taskId);
-    }
-
-    /**
-     * 文件合并
-     * 
-     * @param taskId        任务id
-     * @throws SignatureException 
-     */
-    public static void merge(String taskId) throws SignatureException {
-        String response = HttpUtil.post(LFASR_HOST + MERGE, getBaseAuthParam(taskId));
-        if (response == null) {
-            throw new RuntimeException("文件合并接口请求失败！");
-        }
-        if (JSON.parseObject(response).getInteger("ok") == 0) {
-            System.out.println("文件合并成功, taskId: " + taskId);
-            return;
-        }
-
-        throw new RuntimeException("文件合并失败！" + response);
-    }
-
-    /**
-     * 获取任务进度
-     * 
-     * @param taskId        任务id
-     * @throws SignatureException 
-     */
-    public static ApiResultDto getProgress(String taskId) throws SignatureException {
-        String response = HttpUtil.post(LFASR_HOST + GET_PROGRESS, getBaseAuthParam(taskId));
-        if (response == null) {
-            throw new RuntimeException("获取任务进度接口请求失败！");
-        }
-
-        return JSON.parseObject(response, ApiResultDto.class);
-    }
-
-    /**
-     * 获取转写结果
-     * 
-     * @param taskId
-     * @return
-     * @throws SignatureException 
-     */
-    public static String getResult(String taskId) throws SignatureException {
-        String responseStr = HttpUtil.post(LFASR_HOST + GET_RESULT, getBaseAuthParam(taskId));
-        if (responseStr == null) {
-            throw new RuntimeException("获取结果接口请求失败！");
-        }
-        ApiResultDto response = JSON.parseObject(responseStr, ApiResultDto.class);
-        if (response.getOk() != 0) {
-            throw new RuntimeException("获取结果失败！" + responseStr);
-        }
-
-        return response.getData();
-    }
 }
--- src/test/java/com/chenankj/AppTest.java
+++ src/test/java/com/chenankj/AppTest.java
@@ -3,7 +3,6 @@ package com.chenankj;
 import static org.junit.Assert.assertTrue;
 
 import com.chenankj.aip.ocr.OcrClient;
-import com.chenankj.aip.speech.xunfei.AsrClient;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -28,11 +27,6 @@ public class AppTest
         String path = "E:\\chenankeji\\广东省应急厅\\ocr识别\\7082137cbf89dbddef7384556ea216a.jpg";
         OcrClient ocrClient = new OcrClient();
         JSONObject res = ocrClient.basicGeneral(path,new HashMap<String, String>());
-    }
-
-    @Test
-    public void testXunfeiAsr(){
-       String res =  AsrClient.asr("lfasr.wav");
     }
 
 }
